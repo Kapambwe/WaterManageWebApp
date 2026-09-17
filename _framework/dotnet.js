e4731b0a205fa5b153cbea6e1c9ea00cd9d35497
Local Azure DevOps Emulator <local-azure-devops@localhost>
2026-09-17 19:47:45 +0200

    deploy: CompanyApp.Clients.WaterManagement 2026-09-17 19:47:45

--- _framework/dotnet.js
+++ _framework/dotnet.js
@@ -1308,6 +1308,10 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
     }
   },
   "debugLevel": 0,
+  "appsettings": [
+    "../appsettings.github.json",
+    "../appsettings.json"
+  ],
   "globalizationMode": "sharded",
   "extensions": {
     "blazor": {}
